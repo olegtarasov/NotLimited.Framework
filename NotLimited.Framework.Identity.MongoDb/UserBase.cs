29141ad67e34901f91a9e974d7412a21c8e5fe59
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2014-01-10 08:39:49 +0400

    Added Monge identity ant test web project

---
+++ NotLimited.Framework.Identity.MongoDb/UserBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNet.Identity;
+
+namespace NotLimited.Framework.Identity.MongoDb
+{
+	public class UserBase<TKey> : IUser<TKey>
+	{
+		public TKey Id { get; private set; }
+		public string UserName { get; set; }
+	}
+}
---
+++ NotLimited.Framework.WebTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NotLimited.Framework.WebTest.Startup))]
+namespace NotLimited.Framework.WebTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
