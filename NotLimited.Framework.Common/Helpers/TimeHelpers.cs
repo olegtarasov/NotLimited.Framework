486acde8ca7e87ffe0a3fad26ecc3bd0136256b5
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2014-05-03 16:36:49 +0400

    Time helpers

---
+++ NotLimited.Framework.Common/Helpers/TimeHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NotLimited.Framework.Common.Helpers
+{
+	public static class TimeHelpers
+	{
+		public static DateTime FromUnixTime(this long seconds)
+		{
+			var dateTime = new DateTime(1970, 1, 1);
+			dateTime = dateTime.AddSeconds((double)seconds);
+			return dateTime.ToLocalTime();
+		}
+
+		public static long ToUnixTime(this DateTime dateTime)
+		{
+			return (long)(dateTime - new DateTime(1970, 1, 1)).TotalSeconds;
+		} 
+	}
+}
