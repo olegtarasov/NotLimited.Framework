54af1c8ea32afcab4570be2e5cb5f9475b217293
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2015-01-18 10:21:54 +0300

    Concurrent readonly dictionary

---
+++ NotLimited.Framework.Common/Collections/ConcurrentDictionaryReadonly.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace NotLimited.Framework.Common.Collections
+{
+    public class ConcurrentDictionaryReadonly<TKey, TValue> : ConcurrentDictionary<TKey, TValue>, IReadOnlyDictionary<TKey, TValue>
+    {
+        IEnumerable<TKey> IReadOnlyDictionary<TKey, TValue>.Keys { get { return Keys; } }
+
+        IEnumerable<TValue> IReadOnlyDictionary<TKey, TValue>.Values { get { return Values; } }
+    }
+}
