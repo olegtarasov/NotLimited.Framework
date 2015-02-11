643caba06aa43137e8194cfdbfadd7cd0453a943
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2015-02-11 23:24:54 +0300

    Added colletions

---
+++ NotLimited.Framework.Collections/OrderedMultiValueDictionary.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NotLimited.Framework.Collections
+{
+    public class OrderedMultiValueDictionary<TKey, TValue> : MultiValueDictionary<TKey, TValue>, IEnumerable
+    {
+         private readonly List<TKey> _order = new List<TKey>();
+
+        public new void Add(TKey key, TValue value)
+        {
+            if (!ContainsKey(key))
+            {
+                _order.Add(key);
+            }
+
+            base.Add(key, value);
+        }
+
+        public new void AddRange(TKey key, IEnumerable<TValue> values)
+        {
+            if (!ContainsKey(key))
+            {
+                _order.Add(key);
+            }
+
+            base.AddRange(key, values);
+        }
+
+        public new bool Remove(TKey key)
+        {
+            _order.RemoveAll(x => Equals(x, key));
+            return base.Remove(key);
+        }
+
+        public new bool Remove(TKey key, TValue value)
+        {
+            bool result = base.Remove(key, value);
+            if (!ContainsKey(key))
+            {
+                _order.RemoveAll(x => Equals(x, key));
+            }
+
+            return result;
+        }
+
+        public new IEnumerable<TKey> Keys { get { return _order; } }
+
+        /// <summary>
+        /// This property bears a penalty of ordering all the values every time.
+        /// </summary>
+        public new IEnumerable<IReadOnlyCollection<TValue>> Values
+        {
+            get { return _order.Select(x => this[x]); }
+        }
+
+        public new IEnumerator<KeyValuePair<TKey, IReadOnlyCollection<TValue>>> GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        public void Sort()
+        {
+            _order.Sort();
+        }
+
+        public void SortDescending()
+        {
+            _order.Sort();
+            _order.Reverse();
+        }
+
+        private class Enumerator : IEnumerator<KeyValuePair<TKey, IReadOnlyCollection<TValue>>>
+        {
+            private readonly OrderedMultiValueDictionary<TKey, TValue> _dictionary;
+            private readonly IEnumerator<TKey> _orderEnumerator;
+
+            public Enumerator(OrderedMultiValueDictionary<TKey, TValue> dictionary)
+            {
+                _dictionary = dictionary;
+                _orderEnumerator = _dictionary._order.GetEnumerator();
+            }
+
+            public void Dispose()
+            {
+                _orderEnumerator.Dispose();
+            }
+
+            public bool MoveNext()
+            {
+                return _orderEnumerator.MoveNext();
+            }
+
+            public void Reset()
+            {
+                _orderEnumerator.Reset();
+            }
+
+            public KeyValuePair<TKey, IReadOnlyCollection<TValue>> Current
+            {
+                get
+                {
+                    var key = _orderEnumerator.Current;
+                    return new KeyValuePair<TKey, IReadOnlyCollection<TValue>>(key, _dictionary[key]);
+                }
+            }
+
+            object IEnumerator.Current
+            {
+                get { return Current; } }
+        }
+    }
+}
---
+++ NotLimited.Framework.Collections/PairList.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace NotLimited.Framework.Collections
+{
+    public class Pair<TKey, TValue>
+    {
+        public Pair(TKey key, TValue value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+    }
+
+    public class PairList<TKey, TValue> : List<Pair<TKey, TValue>>
+    {
+        public PairList()
+        {
+        }
+
+        public PairList(IEnumerable<Pair<TKey, TValue>> collection) : base(collection)
+        {
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            Add(new Pair<TKey, TValue>(key, value));
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            return this.Any(x => Equals(x.Key, key));
+        }
+
+        public TValue this[TKey key]
+        {
+            get { return this.First(x => Equals(x.Key, key)).Value; }
+            set
+            {
+                var pair = this.FirstOrDefault(x => Equals(x.Key, key));
+                if (pair != null)
+                {
+                    Remove(pair);
+                }
+
+                Add(key, value);
+            }
+        }
+    }
+}
