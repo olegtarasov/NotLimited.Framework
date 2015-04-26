c2734556e0edd1f12b63a4441420e5ff400d535a
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2015-04-26 08:53:25 +0300

    Updated packages

--- NuGet/PublishCommon.cmd
+++ NuGet/PublishCommon.cmd
@@ -7,5 +7,4 @@ if not exist %nuSource% (
 	"%~dp0..\.nuget\NuGet.exe" "Install" "NuSource" "-OutputDirectory" "%~dp0..\packages" "-ExcludeVersion"
 )
 
-call %nuSource% Publish -s="%solutionPath%" -p="NotLimited.Framework.Common" -i=false -n=false -a=false -f="NotLimited.Framework.Common.Public"
 call %nuSource% Publish -s="%solutionPath%" -p="NotLimited.Framework.Common" -i=false -n=false -f="NotLimited.Framework.Common.PublicPackage"
--- NuGet/PublishWpf.cmd
+++ NuGet/PublishWpf.cmd
@@ -7,5 +7,4 @@ if not exist %nuSource% (
 	"%~dp0..\.nuget\NuGet.exe" "Install" "NuSource" "-OutputDirectory" "%~dp0..\packages" "-ExcludeVersion"
 )
 
-call %nuSource% Publish -s="%solutionPath%" -p="NotLimited.Framework.Wpf" -i=false -n=false -a=false -f="NotLimited.Framework.Wpf.Public"
 call %nuSource% Publish -s="%solutionPath%" -p="NotLimited.Framework.Wpf" -i=false -n=false -f="NotLimited.Framework.Wpf.PublicPackage"
