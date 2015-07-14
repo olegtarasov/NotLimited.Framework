0a15dc5ace0c6d46f904d995a42c3261aac25972
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2015-07-14 18:03:14 +0300

    Added delegate extensions

---
+++ NotLimited.Framework.Common/Helpers/DelegateExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NotLimited.Framework.Common.Helpers
+{
+    public static class DelegateExtensions
+    {
+        public static void InvokeIfNotNull<T>(this Action<T> action, T arg1)
+        {
+            if (action != null)
+            {
+                action(arg1);
+            }
+        }
+
+        public static void InvokeIfNotNull<T1, T2>(this Action<T1, T2> action, T1 arg1, T2 arg2)
+        {
+            if (action != null)
+            {
+                action(arg1, arg2);
+            }
+        }
+    }
+}
