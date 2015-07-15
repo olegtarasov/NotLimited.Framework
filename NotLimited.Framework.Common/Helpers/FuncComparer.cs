eed49abf6397e19cea8d9a702bebf2b2b8efa08e
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2015-07-15 10:16:37 +0300

    Added FuncComparer

---
+++ NotLimited.Framework.Common/Helpers/FuncComparer.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+namespace NotLimited.Framework.Common.Helpers
+{
+    public class FuncComparer<T> : IEqualityComparer<T>
+    {
+        private readonly Func<T, T, bool> _equalityFunc;
+        private readonly Func<T, int> _hashFunc;
+
+        public FuncComparer(Func<T, T, bool> equalityFunc) : this(equalityFunc, arg => arg.GetHashCode())
+        {
+        }
+
+        public FuncComparer(Func<T, T, bool> equalityFunc, Func<T, int> hashFunc)
+        {
+            _equalityFunc = equalityFunc;
+            _hashFunc = hashFunc;
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return _equalityFunc(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return _hashFunc(obj);
+        }
+    }
+}
