86185887310805697741569795be4597500e9195
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2015-08-22 18:06:18 +0300

    Added awaiter

---
+++ NotLimited.Framework.Common/Helpers/Awaiter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace NotLimited.Framework.Common.Helpers
+{
+	public static class Awaiter
+	{
+		public static bool Await(Func<bool> func, TimeSpan timeout)
+		{
+			var startTime = DateTime.Now;
+			while (true)
+			{
+				if (func())
+				{
+					return true;
+				}
+
+				if ((DateTime.Now - startTime) >= timeout)
+				{
+					return false;
+				}
+
+				Thread.Sleep(50);
+			}
+		}
+	}
+}
