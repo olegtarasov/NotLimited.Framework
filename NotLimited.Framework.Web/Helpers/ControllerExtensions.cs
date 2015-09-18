1c5ec31a27456e797bdeccc5dcc92a0f7bbac106
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2015-09-18 11:02:00 +0300

    Added controller helpers

---
+++ NotLimited.Framework.Web/Helpers/ControllerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web.Mvc;
+using Autofac;
+
+namespace NotLimited.Framework.Web.Helpers
+{
+	public static class ControllerExtensions
+	{
+		public static IReadOnlyList<MethodInfo> GetActions(this Type type)
+		{
+			return type.GetMethods(BindingFlags.FlattenHierarchy | BindingFlags.Instance | BindingFlags.Public)
+							.Where(x => x.ReturnType.IsAssignableTo<ActionResult>())
+							.ToList();
+		}
+	}
+}
