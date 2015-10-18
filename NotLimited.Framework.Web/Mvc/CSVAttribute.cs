14a7a2e60268f27fe6ed5b6b1295de927d63421b
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2015-10-18 12:10:55 +0300

    CSV attribute

---
+++ NotLimited.Framework.Web/Mvc/CSVAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace NotLimited.Framework.Web.Mvc
+{
+	public class CSVAttribute : CustomModelBinderAttribute
+	{
+		public override IModelBinder GetBinder()
+		{
+			return new CommaSeparatedModelBinder();
+		}
+	}
+}
