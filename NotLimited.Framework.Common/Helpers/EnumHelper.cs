f86e7ba64c212543b9b8550b083ba3015256d14d
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2016-02-08 18:23:19 +0300

    Fixed packages, added some enum helpers

--- NotLimited.Framework.Common/Helpers/EnumHelper.cs
+++ NotLimited.Framework.Common/Helpers/EnumHelper.cs
@@ -9,16 +9,56 @@ namespace NotLimited.Framework.Common.Helpers
 {
 	public static class EnumHelper
 	{
+		private static Dictionary<Type, Dictionary<string, int>> _valueMap = new Dictionary<Type, Dictionary<string, int>>();
+
+		public static T FromDoubleArray<T>(this double[] array)
+		{
+			var values = GetEnumValues<T>().Select(x => x.Value).OrderBy(x => x).ToList();
+			for (int i = 0; i < array.Length; i++)
+			{
+				if (array[i] > 0)
+				{
+					return (T)(object)values[i];
+				}
+			}
+
+			return default(T);
+		}
+
+		public static double[] ToDoubleArray<T>(this T en)
+		{
+			var values = GetEnumValues<T>().Select(x => x.Value).OrderBy(x => x).ToList();
+			var result = new double[values.Count];
+			result[values.IndexOf(Convert.ToInt32(en))] = 1;
+
+			return result;
+		}
+
 		public static T MergeFlags<T>(this IEnumerable<T> source)
 		{
 			return (T)Enum.ToObject(typeof(T), source.Sum(x => (int)Convert.ChangeType(x, typeof(int))));
 		}
 
+		public static Dictionary<string, int> GetEnumValues<T>()
+		{
+			var type = typeof(T);
+			Dictionary<string, int> result;
+			if (!_valueMap.TryGetValue(type, out result))
+			{
+				result = GetEnumDictionaryReflection<T>();
+				_valueMap[type] = result;
+			}
+
+			return result;
+		}
+
+		[Obsolete]
 		public static Dictionary<T, string> GetEnumDictionary<T>()
 		{
 			return GetEnumDictionary<T>(typeof(T));
 		}
 
+		[Obsolete]
 		public static Dictionary<T, string> GetEnumDictionary<T>(Type enumType)
 		{
 			var fields = enumType.GetFields(BindingFlags.Public | BindingFlags.Static);
@@ -34,6 +74,18 @@ namespace NotLimited.Framework.Common.Helpers
 		public static string GetEnumValueString<T>(this T en)
 		{
 			return GetEnumDictionary<T>()[en];
+		}
+
+		public static Dictionary<string, int> GetEnumDictionaryReflection<T>()
+		{
+			var fields = typeof(T).GetFields(BindingFlags.Public | BindingFlags.Static);
+
+			return (from field in fields
+					let value = field.GetRawConstantValue()
+					let description = field.GetDisplayName()
+					orderby value
+					select new { Name = description, Value = value })
+				.ToDictionary(x => x.Name, x => (int)x.Value);
 		}
 	}
 }
---
+++ NotLimited.Framework.Tests/Common/Helpers/EnumHelperTests.cs
@@ -0,0 +1,24 @@
+﻿using NotLimited.Framework.Common.Helpers;
+using Xunit;
+using XunitShould;
+
+namespace NotLimited.Framework.Tests.Common.Helpers
+{
+	public enum TestDefault
+	{
+		None = -1,
+		One = 1,
+		Two = 3,
+		Three = 2
+	}
+
+	public class EnumHelperTests
+	{
+		[Fact]
+		public void CanConvertEnumValueToArray()
+		{
+			var result = TestDefault.Three.ToDoubleArray();
+			result.ShouldEnumerateEqual(0, 0, 1, 0);
+		}
+	}
+}
