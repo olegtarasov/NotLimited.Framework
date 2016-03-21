c8796ed8531297162f3536656b5029a2ee1f6dca
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2016-03-21 21:11:09 +0300

    MinMaxTracker

---
+++ NotLimited.Framework.Common/Helpers/MinMaxTracker.cs
@@ -0,0 +1,24 @@
+﻿namespace NotLimited.Framework.Common.Helpers
+{
+	public class MinMaxTracker
+	{
+		public double Min = double.MaxValue;
+		public double Max = double.MinValue;
+
+		public void Track(params double[] numbers)
+		{
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				if (numbers[i] > Max)
+				{
+					Max = numbers[i];
+				}
+
+				if (numbers[i] < Min)
+				{
+					Min = numbers[i];
+				}
+			}
+		}
+	}
+}
