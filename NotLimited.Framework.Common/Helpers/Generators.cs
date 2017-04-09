f497b1dcd2f20fa2b9497f9df2a443309801db85
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2017-04-09 10:24:01 +0300

    Added generators

---
+++ NotLimited.Framework.Common/Helpers/Generators.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace NotLimited.Framework.Common.Helpers
+{
+    public static class Generators
+    {
+        public static Dictionary<TKey, TValue> dict<TKey, TValue, TSrc>(
+            IEnumerable<TSrc> source, Func<TSrc, TKey> keyFunc, Func<TSrc, TValue> valueFunc)
+        {
+            return source.ToDictionary(keyFunc, valueFunc);
+        }
+
+        public static Dictionary<TKey, TValue> dict<TKey, TValue>()
+        {
+            return new Dictionary<TKey, TValue>();
+        }
+
+        public static FileStream ofstream(string path, FileMode mode = FileMode.Create)
+        {
+            return new FileStream(path, mode, FileAccess.Write);
+        }
+
+        public static StreamWriter swriter(string path)
+        {
+            return new StreamWriter(path);
+        }
+    }
+}
