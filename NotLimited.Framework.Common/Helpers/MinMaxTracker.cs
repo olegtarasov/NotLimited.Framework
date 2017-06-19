3afb3bef28bb299d159b181db296e41b2e83cd7f
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2017-06-19 10:37:23 +0300

    Commented minmaxtracker

--- NotLimited.Framework.Common/Helpers/MinMaxTracker.cs
+++ NotLimited.Framework.Common/Helpers/MinMaxTracker.cs
@@ -1,45 +1,45 @@
-﻿namespace NotLimited.Framework.Common.Helpers
-{
-	public class MinMaxTracker
-	{
-		public double Min { get; private set; } = double.NaN;
-		public double Max { get; private set; } = double.NaN;
+﻿//namespace NotLimited.Framework.Common.Helpers
+//{
+//	public class MinMaxTracker
+//	{
+//		public double Min { get; private set; } = double.NaN;
+//		public double Max { get; private set; } = double.NaN;
 
-	    public double Range { get; private set; } = double.NaN;
+//	    public double Range { get; private set; } = double.NaN;
 
-		public void Track(params double[] numbers)
-		{
-			for (int i = 0; i < numbers.Length; i++)
-			{
-			    if (double.IsNaN(numbers[i]))
-			    {
-			        continue;
-			    }
+//		public void Track(params double[] numbers)
+//		{
+//			for (int i = 0; i < numbers.Length; i++)
+//			{
+//			    if (double.IsNaN(numbers[i]))
+//			    {
+//			        continue;
+//			    }
 
-				if (double.IsNaN(Max) || numbers[i] > Max)
-				{
-					Max = numbers[i];
-				}
+//				if (double.IsNaN(Max) || numbers[i] > Max)
+//				{
+//					Max = numbers[i];
+//				}
 
-				if (double.IsNaN(Min) || numbers[i] < Min)
-				{
-					Min = numbers[i];
-				}
+//				if (double.IsNaN(Min) || numbers[i] < Min)
+//				{
+//					Min = numbers[i];
+//				}
 
-			    Range = Max - Min;
-			}
-		}
+//			    Range = Max - Min;
+//			}
+//		}
 
-	    public double Normalize(double value)
-	    {
-	        return (value - Min) / Range;
-	    }
+//	    public double Normalize(double value)
+//	    {
+//	        return (value - Min) / Range;
+//	    }
 
-	    public void Reset()
-	    {
-	        Min = double.MaxValue;
-	        Max = double.MinValue;
-	        Range = double.NaN;
-	    }
-	}
-}
+//	    public void Reset()
+//	    {
+//	        Min = double.MaxValue;
+//	        Max = double.MinValue;
+//	        Range = double.NaN;
+//	    }
+//	}
+//}
