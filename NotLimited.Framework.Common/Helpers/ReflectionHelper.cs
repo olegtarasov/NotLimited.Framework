52fa50135cfb4b620b6554e00af3df41c2331208
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2018-03-10 12:32:59 +0300

    Common to NET Standard

--- NotLimited.Framework.Common/Helpers/ReflectionHelper.cs
+++ NotLimited.Framework.Common/Helpers/ReflectionHelper.cs
@@ -1,52 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.Emit;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Reflection;
+//using System.Reflection.Emit;
 
-namespace NotLimited.Framework.Common.Helpers
-{
-	public static class ReflectionHelper
-	{
-		public static CustomAttributeBuilder ToAttributeBuilder(this CustomAttributeData data)
-		{
-			if (data == null)
-				throw new ArgumentNullException("data");
+//namespace NotLimited.Framework.Common.Helpers
+//{
+//	public static class ReflectionHelper
+//	{
+//		public static CustomAttributeBuilder ToAttributeBuilder(this CustomAttributeData data)
+//		{
+//			if (data == null)
+//				throw new ArgumentNullException("data");
 
-			var constructorArguments = data.ConstructorArguments.Select(x => x.Value).ToList();
-			var propertyArguments = new List<PropertyInfo>();
-			var propertyArgumentValues = new List<object>();
-			var fieldArguments = new List<FieldInfo>();
-			var fieldArgumentValues = new List<object>();
+//			var constructorArguments = data.ConstructorArguments.Select(x => x.Value).ToList();
+//			var propertyArguments = new List<PropertyInfo>();
+//			var propertyArgumentValues = new List<object>();
+//			var fieldArguments = new List<FieldInfo>();
+//			var fieldArgumentValues = new List<object>();
 
-			if (data.NamedArguments != null)
-			{
-				foreach (var namedArg in data.NamedArguments)
-				{
-					var fi = namedArg.MemberInfo as FieldInfo;
-					var pi = namedArg.MemberInfo as PropertyInfo;
+//			if (data.NamedArguments != null)
+//			{
+//				foreach (var namedArg in data.NamedArguments)
+//				{
+//					var fi = namedArg.MemberInfo as FieldInfo;
+//					var pi = namedArg.MemberInfo as PropertyInfo;
 
-					if (fi != null)
-					{
-						fieldArguments.Add(fi);
-						fieldArgumentValues.Add(namedArg.TypedValue.Value);
-					}
-					else if (pi != null)
-					{
-						propertyArguments.Add(pi);
-						propertyArgumentValues.Add(namedArg.TypedValue.Value);
-					}
-				}
-			}
+//					if (fi != null)
+//					{
+//						fieldArguments.Add(fi);
+//						fieldArgumentValues.Add(namedArg.TypedValue.Value);
+//					}
+//					else if (pi != null)
+//					{
+//						propertyArguments.Add(pi);
+//						propertyArgumentValues.Add(namedArg.TypedValue.Value);
+//					}
+//				}
+//			}
 
-			return new CustomAttributeBuilder(
-				data.Constructor,
-				constructorArguments.ToArray(),
-				propertyArguments.ToArray(),
-				propertyArgumentValues.ToArray(),
-				fieldArguments.ToArray(),
-				fieldArgumentValues.ToArray());
-		}
+//			return new CustomAttributeBuilder(
+//				data.Constructor,
+//				constructorArguments.ToArray(),
+//				propertyArguments.ToArray(),
+//				propertyArgumentValues.ToArray(),
+//				fieldArguments.ToArray(),
+//				fieldArgumentValues.ToArray());
+//		}
 
-	}
-}
+//	}
+//}
