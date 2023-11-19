a030a526096b623d90b902344239b5772a3fa6db
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2023-11-19 19:06:29 +0300

    Enhanced ProcessHelper

--- NotLimited.Framework.Common/Helpers/ProcessHelper.cs
+++ NotLimited.Framework.Common/Helpers/ProcessHelper.cs
@@ -11,7 +11,7 @@ public static class ProcessHelper
     /// <summary>
     /// Runs a process synchronously and captures its output and optionally error stream.
     /// </summary>
-    public static async Task<string> RunAndGetOutput(string fileName, string arguments = "", string workDir = "", bool includeError = false)
+    public static async Task<OutputLine[]> RunAndGetOutput(string fileName, string arguments = "", string workDir = "", bool includeError = false)
     {
         var info = new ProcessStartInfo(fileName, arguments)
                    {
@@ -20,19 +20,56 @@ public static class ProcessHelper
                        WorkingDirectory = workDir
                    };
         var process = Process.Start(info) ?? throw new InvalidOperationException("Failed to start the process");
-        var builder = new StringBuilder();
-        
-        process.OutputDataReceived += (sender, args) => builder.AppendLine(args.Data);
+        var result = new List<OutputLine>();
+
+        process.OutputDataReceived += (sender, args) =>
+        {
+            if (args.Data != null)
+                result.Add(new(OutputType.Output, args.Data));
+        };
         process.BeginOutputReadLine();
 
         if (includeError)
         {
-            process.ErrorDataReceived += (sender, args) => builder.AppendLine(args.Data);
+            process.ErrorDataReceived += (sender, args) =>
+            {
+                if (args.Data != null)
+                    result.Add(new(OutputType.Error, args.Data));
+            };
             process.BeginErrorReadLine();
         }
 
         await process.WaitForExitAsync();
 
-        return builder.ToString();
+        return result.ToArray();
+    }
+
+    /// <summary>
+    /// Output line with type.
+    /// </summary>
+    /// <param name="Type">Stream type.</param>
+    /// <param name="Text">Text.</param>
+    public record OutputLine(OutputType Type, string Text)
+    {
+        /// <summary>
+        /// Returns line text
+        /// </summary>
+        public static implicit operator string(OutputLine line) => line.Text;
+    }
+    
+    /// <summary>
+    /// Output line type
+    /// </summary>
+    public enum OutputType
+    {
+        /// <summary>
+        /// stdout
+        /// </summary>
+        Output,
+        
+        /// <summary>
+        /// stderr
+        /// </summary>
+        Error
     }
 }
